3b7d256ad3eb5c81ac5b9c5a0cd2c026c6a47ecf
valarmathigithub03 <165669364+valarmathigithub03@users.noreply.github.com>
2024-04-10 17:50:46 +0530

    Update apps.py

--- hello_azure/apps.py
+++ hello_azure/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class HelloAzureConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hello_azure'
+    name = 'hello_valar'
 
